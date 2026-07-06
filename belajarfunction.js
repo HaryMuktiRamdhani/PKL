e1db2981ae551b45e32b3a6696c03e1f747c631d
Hary Mukti Ramdhani <harymukti757@gmail.com>
2026-07-06 16:50:55 +0700

    tugas

---
+++ belajarfunction.js
@@ -0,0 +1,18 @@
+function luassegitiga(alas, tinggi) {
+    return (alas * tinggi) / 2;
+}
+console.log("Luas segitiga:", luassegitiga(10, 5));
+
+
+function luaslingkaran(jarijari) {
+    return Math.PI * jarijari * jarijari; 
+}
+console.log("luas lingkaran:", luaslingkaran(7).toFixed(2));
+
+
+function luaspermukaankubus(sisi) {
+    return 6 * sisi * sisi;
+}
+console.log("luas permukaan kubus:", luaspermukaankubus(4));
+
+ 
